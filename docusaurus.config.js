485bfa5887e017f5713dc388a7236be25312e78e
Emmanuel Korir <emmanuelqkorir@gmail.com>
2022-10-30 20:21:13 +0300

    add last update feature

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
 
         gtag: {
