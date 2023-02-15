dc49fce335d25ebae6140da6127be3f861ab7010
Emmanuel Korir <emmanuelqkorir@gmail.com>
2023-02-15 21:04:11 +0300

    add description to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="For MBChB students at the University of Nairobi."
     >
       <HomepageHeader />
       <main>
